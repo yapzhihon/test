d52532cd46d22723c05dd256d79698d63ad680cf
iiiiio <y.zhihon@gmail.com>
2019-07-08 22:08:00 +0800

    Added list, adjusted models to conform to diff lists

--- lists/views.py
+++ lists/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
-from lists.models import Item
+from lists.models import Item, List
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
