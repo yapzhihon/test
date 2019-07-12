9659da317d6fdfee3a4c31314a528c4d4e61c934
iiiiio <y.zhihon@gmail.com>
2019-07-12 13:16:01 +0800

    Minor changes

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from django.utils.html import escape
 
-from unittest import skip
 
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 from lists.models import Item, List
-
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm
+)
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -125,7 +126,6 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -133,7 +133,7 @@ class ListViewTest(TestCase):
             f'/lists/{list1.id}/',
             data={'text': 'textey'}
         )
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
