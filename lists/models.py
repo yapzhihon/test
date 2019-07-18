53d0d4b52d9ef2cb28aa5238300b4bfa9e2110df
iiiiio <y.zhihon@gmail.com>
2019-07-18 14:48:04 +0800

    lists can have owners, which are saved on creation

--- lists/models.py
+++ lists/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 class List(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
+
     class Meta:
         ordering = ('id',)
         unique_together = ('list', 'text')
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -49,6 +49,13 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_list_owner_is_saved_if_user_is_authenticated(self):
+        user = User.objects.create(email='a@b.com')
+        self.client.force_login(user)
+        self.client.post('/lists/new', data={'text': 'new item'})
+        list_ = List.objects.first()
+        self.assertEqual(list_.owner, user)
+
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         list_ = List.objects.create()
