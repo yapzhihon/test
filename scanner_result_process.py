25467312d3d35c065dce17925fed1f58b1e92165
zhihon <zhihon.yap@gmail.com>
2019-09-30 16:06:03 +0800

    changed travis api_key

--- scanner_result_process.py
+++ scanner_result_process.py
@@ -1,12 +1,20 @@
 import requests
+import json
 
 from django.conf import settings
+
 
 def main():
     data = {'projectKey': settings.SONAR_PROJECTKEY}
     url = settings.SONAR_URL + '/api/qualitygates/project_status'
     request = requests.get(url=url, params=data)
-    print(request.content)
+
+    print(request.status_code)
+    if request.status_code == 200:
+        print(request.content)
+        data = json.loads(request.content)
+        project_status = data.get('projectStatus', {})
+        print(project_status)
 
 
 if __name__ == '__main__':
