b2ad35877b114fe76dc53b738e7e88f9a8a75771
zhihon <zhihon.yap@gmail.com>
2019-09-30 16:37:05 +0800

    fail with scanner

--- scanner_result_process.py
+++ scanner_result_process.py
@@ -5,6 +5,11 @@ from django.conf import settings
 
 
 def main():
+    SUCCESS = 'OK'
+    WARNING = 'WARN'
+    ERROR = 'ERROR'
+    NONE = 'NONE'
+
     data = {'projectKey': settings.SONAR_PROJECTKEY}
     url = settings.SONAR_URL + '/api/qualitygates/project_status'
     request = requests.get(url=url, params=data)
@@ -14,7 +19,16 @@ def main():
         print(request.content)
         data = json.loads(request.content)
         project_status = data.get('projectStatus', {})
-        print(project_status)
+        status = project_status.get('status', '')
+
+        if status == SUCCESS:
+            return
+        elif status == WARNING or status == ERROR or status == NONE:
+            return 1
+        else:
+            return 1
+    else:
+        return 1
 
 
 if __name__ == '__main__':
