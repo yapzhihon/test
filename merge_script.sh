db25989a15e3da9982ff819aa26949c5f7dd8763
zhihon <zhihon.yap@gmail.com>
2019-10-01 15:29:01 +0800

    test merge

--- merge_script.sh
+++ merge_script.sh
@@ -2,9 +2,16 @@ if [ "$TRAVIS_BRANCH" != "test" ]; then
     exit 0;
 fi
 
+echo "here"
+
 export GIT_COMMITTER_EMAIL=''
 export GIT_COMMITTER_NAME=''
 
+echo "here 2"
+
 git checkout master || exit
+echo "here 3"
 git merge "$TRAVIS_COMMIT" || exit
+echo "4"
 git push
+echo "done"
