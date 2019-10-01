810da2f8604bb6a566fd3ea84d65fb819d8c4a0f
zhihon <zhihon.yap@gmail.com>
2019-10-01 17:20:37 +0800

    try to merge from bash

--- merge_script.sh
+++ merge_script.sh
@@ -1,12 +1,12 @@
 echo "sd"
 
 echo "$TRAVIS_BRANCH"
-if [ "$TRAVIS_BRANCH" != "test" ]; then
-    exit 0;
-    echo "stuff"
-fi
-
-echo "here"
+#if [ "$TRAVIS_BRANCH" != "test" ]; then
+#    exit 0;
+#    echo "stuff"
+#fi
+#
+#echo "here"
 
 export GIT_COMMITTER_EMAIL=''
 export GIT_COMMITTER_NAME=''
