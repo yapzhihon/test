188d9b9ac2cb25b6e521e878ceb4838c2e7e0831
zhihon <zhihon.yap@gmail.com>
2019-10-04 14:43:20 +0800

    check

--- merge_script.sh
+++ merge_script.sh
@@ -1,4 +1,4 @@
-echo "$TRAVIS_BRANCH"
+#echo "$TRAVIS_BRANCH"
 #if [ "$TRAVIS_BRANCH" != "test" ]; then
 #    exit 0;
 #    echo "stuff"
@@ -6,19 +6,21 @@ echo "$TRAVIS_BRANCH"
 #
 #echo "here"
 
-export GIT_COMMITTER_EMAIL=''
-export GIT_COMMITTER_NAME=''
-echo "$TRAVIS_REPO_SLUG"
-export GIT_TOKEN='57ab2e92d63d5f0a81483825d95ebac17d43b5c1'
-
-echo "here 2"
+#export GIT_COMMITTER_EMAIL=''
+#export GIT_COMMITTER_NAME=''
+#echo "$TRAVIS_REPO_SLUG"
+#export GIT_TOKEN='57ab2e92d63d5f0a81483825d95ebac17d43b5c1'
+#
+#echo "here 2"
 
 git branch -d master || true
-git checkout --detach
-git fetch origin '+refs/heads/*:refs/heads/origin/*'
-git checkout --track origin/master || exit
-echo "here 3"
-git merge "$TRAVIS_COMMIT" || exit
-echo "4"
-git push
-echo "done"
+#git checkout --detach
+#git fetch origin '+refs/heads/*:refs/heads/origin/*'
+git fetch --all
+git branch -a
+#git checkout --track origin/master || exit
+#echo "here 3"
+#git merge "$TRAVIS_COMMIT" || exit
+#echo "4"
+#git push
+#echo "done"
