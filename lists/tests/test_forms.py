b37295c51e7fd7bed78c3b733b537c235799336f
zhihon <zhihon.yap@gmail.com>
2019-10-09 19:25:33 +0800

    allow fail

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -79,3 +79,6 @@ class NewListFormTest(unittest.TestCase):
         form.is_valid()
         response = form.save(owner=user)
         self.assertEqual(response, mock_List_create_new.return_value)
+
+    def test_fail(self):
+        self.fail()
