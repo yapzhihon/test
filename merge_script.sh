323e04dc143fc293d956d662a74a90a4f74287d1
zhihon <zhihon.yap@gmail.com>
2019-10-14 11:08:22 +0800

    try deploy only

--- merge_script.sh
+++ merge_script.sh
@@ -4,16 +4,28 @@
 #fi
 
 #git branch -d master || true
+echo "git branch -a"
 git branch -a
+
+echo "git checkout --detach"
 git checkout --detach
+
+echo "git fetch origin '+refs/heads/*:refs/heads/*'"
 git fetch origin '+refs/heads/*:refs/heads/*'
+
+echo "git fetch --all"
 git fetch --all
+
+echo "git branch -a"
 git branch -a
 #git checkout $TRAVIS_BRANCH || exit
 
 #git merge $TRAVIS_COMMIT || exit
+
+echo "git checkout $TRAVIS_BRANCH"
 git checkout $TRAVIS_BRANCH
 
+echo "git merge $TRAVIS_COMMIT"
 git merge $TRAVIS_COMMIT
 exit 1
 
