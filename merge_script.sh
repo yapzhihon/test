f9744d9200057e8b919735d8c7a0db19d843edea
zhihon <zhihon.yap@gmail.com>
2019-10-14 10:53:02 +0800

    try deploy

--- merge_script.sh
+++ merge_script.sh
@@ -1,18 +1,22 @@
-if [ "$TRAVIS_BRANCH" = "staging" ]; then
-  echo "trigerred"
-  exit 0;
-fi
+#if [ "$TRAVIS_BRANCH" = "staging" ]; then
+#  echo "trigerred"
+#  exit 0;
+#fi
 
-git branch -d master || true
+#git branch -d master || true
+git branch -a
 git checkout --detach
 git fetch origin '+refs/heads/*:refs/heads/*'
 git fetch --all
 git branch -a
-git checkout master || exit
-#echo "here 3"
-git merge "$TRAVIS_COMMIT" || exit
+#git checkout $TRAVIS_BRANCH || exit
+
+#git merge $TRAVIS_COMMIT || exit
+git checkout $TRAVIS_BRANCH
+
+git merge $TRAVIS_COMMIT
 exit 1
 
-git push https://$GIT_USERNAME:$GIT_PWD@github.com/$TRAVIS_REPO_SLUG.git
+#git push https://$GIT_USERNAME:$GIT_PWD@github.com/$TRAVIS_REPO_SLUG.git
 #git push --set-upstream origin master
 #echo "done"
